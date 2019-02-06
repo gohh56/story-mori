519a37eb0b82ee2261ed08c67c835a9319c9253c
jonatan <jonatin@gmail.com>
2019-02-06 21:13:44 +0900

    add story create

--- server/setup.js
+++ server/setup.js
@@ -1,11 +1,38 @@
 const db = require('./db/connection');
 
 const setup = function(app) {
-  app.use('/api/stories', async (req, res, next) => {
+  app.get('/api/stories', async (req, res, next) => {
     const connection = await db.getConnection();
     const [results] = await connection.query('select * from stories');
 
     res.json(results);
+  });
+
+  app.post('/api/stories', async (req, res, next) => {
+    // get parameters
+    const { title, text } = req.body;
+
+    // save story in database
+    const connection = await db.getConnection();
+    const insertStoryQuery =
+      'insert into stories (title, summary) values (?, ?)';
+    const [storyResults] = await connection.query(insertStoryQuery, [
+      title,
+      text.slice(0, 10)
+    ]);
+    const storyId = storyResults.insertId;
+
+    // save first page in database
+    const insertPageQuery =
+      'insert into pages (name, text, story_id) values (?, ?, ?)';
+    const [pageResults] = await connection.query(insertPageQuery, [
+      title,
+      text,
+      storyId
+    ]);
+
+    // return story id / story object
+    res.json({ id: storyId });
   });
 };
 
--- src/lib/api-service.js
+++ src/lib/api-service.js
@@ -2,3 +2,15 @@ export const getStories = async function() {
   const resp = await fetch("/api/stories");
   return resp.json();
 }
+
+export const saveStory = async function(story) {
+  const resp = await fetch("/api/stories", {
+    method: "post",
+    body: JSON.stringify(story),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    }
+  });
+
+  return resp.json();
+}
