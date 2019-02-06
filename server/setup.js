34f3838b376a8d63ffa13f1601349c6d3e639461
jonatan <jonatin@gmail.com>
2019-02-06 21:29:52 +0900

    refactor apis into controller files

--- server/setup.js
+++ server/setup.js
@@ -1,39 +1,8 @@
-const db = require('./db/connection');
+const { getStories, saveStory } = require('./controller/stories');
 
 const setup = function(app) {
-  app.get('/api/stories', async (req, res, next) => {
-    const connection = await db.getConnection();
-    const [results] = await connection.query('select * from stories');
-
-    res.json(results);
-  });
-
-  app.post('/api/stories', async (req, res, next) => {
-    // get parameters
-    const { title, text } = req.body;
-
-    // save story in database
-    const connection = await db.getConnection();
-    const insertStoryQuery =
-      'insert into stories (title, summary) values (?, ?)';
-    const [storyResults] = await connection.query(insertStoryQuery, [
-      title,
-      text.slice(0, 10)
-    ]);
-    const storyId = storyResults.insertId;
-
-    // save first page in database
-    const insertPageQuery =
-      'insert into pages (name, text, story_id) values (?, ?, ?)';
-    const [pageResults] = await connection.query(insertPageQuery, [
-      title,
-      text,
-      storyId
-    ]);
-
-    // return story id / story object
-    res.json({ id: storyId });
-  });
+  app.get('/api/stories', getStories);
+  app.post('/api/stories', saveStory);
 };
 
 module.exports = setup;
